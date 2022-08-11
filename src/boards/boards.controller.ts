95628e220bba3a8d8e5a4658230579dd72bc6bed
leedongyeop <ldy_1204@naver.com>
2022-08-12 00:19:06 +0900

    board controller

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoard(@Param('id') id: string, @Body('status') status: BoardStatus) {
+    return this.boardsService.updateBoardStatus(id, status);
+  }
+}
