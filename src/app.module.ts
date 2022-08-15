d21afd860256de2c5a923cec6333f4f0f7a42128
leedongyeop <ldy_1204@naver.com>
2022-08-15 15:20:04 +0900

    project 시작점

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 
+/* 앱 모듈 - 가장 기본이 되는 모듈 */
+
 @Module({
   imports: [BoardsModule],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/* 프로젝트 시작점 */
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
