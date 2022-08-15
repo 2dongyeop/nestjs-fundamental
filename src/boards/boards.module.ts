1223ad1b24f57d9c76d4695dd5dac038ce282d38
leedongyeop <ldy_1204@naver.com>
2022-08-15 15:43:39 +0900

    Provider 등록

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
-//nest cli를 이용하면 아래와 같이 모듈 등록은 자동으로 됨
+//nest cli를 이용하면 아래와 같이 프로바이더 등록은 자동으로 됨
 @Module({
   controllers: [BoardsController],
   providers: [BoardsService],
