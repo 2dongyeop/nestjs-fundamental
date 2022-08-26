baa82bf926e7c91899b7ae91d2297704e01a8e88
leedongyeop <ldy_1204@naver.com>
2022-08-26 17:48:21 +0900

    Pipe : Data Transform & Data validation

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,37 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+/**
+ * 유효성 검사 파이프 만들기
+ */
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+
+  transform(value: string, metadata: ArgumentMetadata): any {
+    /**
+     * Data Transform : 입력 데이터를 원하는 형식으로변환
+     */
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} is not in the status options`);
+    }
+    return value;
+  }
+
+  private isStatusValid(status: any): boolean {
+    const index = this.StatusOptions.indexOf(status);
+
+    /**
+     * Data validation : 입력 데이터의 유효성 체크
+     *  status가 PUBLIC, PRIVATE가 아닌 값이 들어오면 에러 발생
+     */
+
+    return index !== -1;
+  }
+}
