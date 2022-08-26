26c0e25c134fc544b3cd3fea0eee8ad866ee1f36
leedongyeop <ldy_1204@naver.com>
2022-08-26 17:55:35 +0900

    @IsNotEmpty로 입력값이 있는지 유효성 체크 추가

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,9 +1,14 @@
-/*
-DTO : Data Transfer Object
-계층간 데이터 교환을 위한 객체로,
-DB에서 data를 얻어 Service나 Controller 등으로 보낼 떄 사용한다.
+/**
+ * DTO : Data Transfer Object
+ * 계층간 데이터 교환을 위한 객체로,
+ * DB에서 data를 얻어 Service나 Controller 등으로 보낼 때 사용한다.
  */
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateBoardDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
