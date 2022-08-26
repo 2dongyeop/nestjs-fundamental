b0230bb9ed9f93d821ca503dc3b286716e3d580f
leedongyeop <ldy_1204@naver.com>
2022-08-26 17:53:20 +0900

    handler-lavel pipe를 controller에 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -6,10 +6,13 @@ import {
   Param,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 //@Controller 데코레이터를 사용하여 boards controller 생성
 @Controller('boards')
@@ -28,6 +31,7 @@ export class BoardsController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe) //handler-level pipe
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
   }
@@ -38,7 +42,10 @@ export class BoardsController {
   }
 
   @Patch('/:id/status')
-  updateBoard(@Param('id') id: string, @Body('status') status: BoardStatus) {
+  updateBoard(
+    @Param('id') id: string,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  ) {
     return this.boardsService.updateBoardStatus(id, status);
   }
 }
