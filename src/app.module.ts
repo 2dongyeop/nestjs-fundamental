aeeb4fcbefce0364fad8a6785b6338182822fece
leedongyeop <ldy_1204@naver.com>
2022-08-29 21:45:39 +0900

    Type ORM 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './configs/typeorm.config';
 
 /* 앱 모듈 - 가장 기본이 되는 모듈 */
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), BoardsModule],
 })
 export class AppModule {}
