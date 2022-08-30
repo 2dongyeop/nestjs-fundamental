361a37491f285c2b3c0734da812030d8883d20e5
leedongyeop <ldy_1204@naver.com>
2022-08-30 12:27:50 +0900

    Entity file

---
+++ src/boards/board.entity.ts
@@ -0,0 +1,22 @@
+import { BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { BoardStatus } from './board.model';
+
+/**
+ * Entity를 생성해야 하는 이유
+ * TypeORM을 사용하면 데이터베이스 테이블로 변환 되는 Class이기에
+ * 간단하게 컬럼들만 정의하면 된다.
+ */
+
+export class Board extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: BoardStatus;
+}
