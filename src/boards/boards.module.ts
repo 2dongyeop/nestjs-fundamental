f2057d43112ceecca62f8e026e5727a45c654c53
leedongyeop <ldy_1204@naver.com>
2022-08-30 13:42:08 +0900

    Repository : Entity 찾기, 삽입, 업데이트 등을 담당

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
 
 //nest cli를 이용하면 아래와 같이 프로바이더 등록은 자동으로 됨
 @Module({
+  imports: [TypeOrmModule.forFeature([BoardRepository])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
