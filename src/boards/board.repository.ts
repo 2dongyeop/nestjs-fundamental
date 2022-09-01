ec69d3ed9a15243dedd797f1d6987116ad24ffec
leedongyeop <ldy_1204@naver.com>
2022-09-01 15:06:47 +0900

    @EntityRepository()가 deprecated 되어 삭제

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,5 +1,9 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Board } from './board.entity';
 
-@EntityRepository(Board)
+/**
+ * @EntityRepository(): 클래스를 사용자 정의(CUSTOM) 저장소로 선언
+ * 현재는 deprecated 됨
+ */
+// @EntityRepository(Board)
 export class BoardRepository extends Repository<Board> {}
